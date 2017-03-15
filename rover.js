f4f12fea47bce11d98c54ef92a543802ac44c232
Carlos Luis <clburguete@gmail.com>
2017-03-15 11:54:28 +0100

    done

--- rover.js
+++ rover.js
@@ -1,25 +1,106 @@
+var instructions = " ";
+var length;
+
+var myGrid = new Array(10);
+for (i = 0; i < 10; i++)
+  myGrid[i] = new Array(10);
+
+
+
 var myRover = {
-  position: [0,0], 
+  position: [0,0],
   direction: 'N'
 };
 
-function goForward(rover) {
-  switch(rover.direction) {
+function rotateRight(myRover) {
+  switch(myRover.direction) {
     case 'N':
-      rover.position[0]++
+      myRover.direction = 'E';
       break;
     case 'E':
-      rover.position[1]++
+      myRover.direction = 'S';
       break;
     case 'S':
-      rover.position[0]--
+      myRover.direction = 'W';
       break;
     case 'W':
-      rover.position[1]--
+      myRover.direction = 'N';
       break;
-  };
+  }
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+}
+function rotateLeft(myRover) {
+  switch(myRover.direction) {
+    case 'N':
+      myRover.direction = 'W';
+      break;
+    case 'E':
+      myRover.direction = 'N';
+      break;
+    case 'S':
+      myRover.direction = 'E';
+      break;
+    case 'W':
+      myRover.direction = 'S';
+      break;
+  }
+
 }
 
-goForward(myRover);
+function goForward(myRover) {
+  switch(myRover.direction) {
+    case 'N':
+      myRover.position[0]++;
+      break;
+    case 'E':
+      myRover.position[1]++;
+      break;
+    case 'S':
+      myRover.position[0]--;
+      break;
+    case 'W':
+      myRover.position[1]--;
+      break;
+  }
+
+}
+
+function goBackward(myRover) {
+    switch(myRover.direction) {
+      case 'N':
+        myRover.position[0]--;
+        break;
+      case 'E':
+        myRover.position[1]--;
+        break;
+      case 'S':
+        myRover.position[0]++;
+        break;
+      case 'W':
+        myRover.position[1]++;
+        break;
+    }
+
+
+}
+
+for(length = 0; length < instructions.length; length ++){
+  switch (instructions.charAt(length)){
+    case "f":
+      goForward(myRover);
+      break;
+    case "b":
+      goBackward(myRover);
+      break;
+    case "r":
+      rotateRight(myRover);
+      break;
+    case "f":
+      rotateLeft(myRover);
+      break;
+  }
+}
+
+
+
+//goForward(myRover);
